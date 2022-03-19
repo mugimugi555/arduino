372cfe47e108f926964290dcee819e4d3715b12b
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-19 22:34:40 +0900

    Update bmp180_web.ino

--- eps32/bmp180/bmp180_web.ino
+++ eps32/bmp180/bmp180_web.ino
@@ -1,6 +1,5 @@
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
-#define seaLevelPressure_hPa 1013.25
 Adafruit_BMP085 bmp;
 
 #include <WiFi.h>
@@ -8,8 +7,8 @@ Adafruit_BMP085 bmp;
 #include <WebServer.h>
 #include <ESPmDNS.h>
 
-const char* ssid = "************";
-const char* password = "*********";
+const char* ssid     = "**********";
+const char* password = "**********";
 
 WebServer server(80);
 
@@ -29,58 +28,45 @@ void handleRoot() {
   message += ",";
     
   message += "\"pressure\":{\"value\":";
-  message += bmp.readPressure() / 100.0F;
+  message += bmp.readPressure() / 100.0f;
   message += ",\"unit\":\"hPa\"}";
 
   message += ",";
-  
+      
   message += "\"altitude\":{\"value\":";
   message += bmp.readAltitude();
   message += ",\"unit\":\"meters\"}";
 
-  message += ",";  
-
-  message += "\"sealevel\":{\"value\":";
-  message += bmp.readSealevelPressure() / 100.0F;
-  message += ",\"unit\":\"hPa\"}";
-
   message += ",";
     
-  message += "\"altitude\":{\"value\":";
-  message += bmp.readAltitude(seaLevelPressure_hPa * 100);
-  message += ",\"unit\":\"meter\"}";
+  message += "\"sealevel\":{\"value\":";
+  message += bmp.readSealevelPressure() / 100.0f;
+  message += ",\"unit\":\"hPa\"}";
 
   message += "}";
 
-  server.send(200, "application/json", message );
+  server.send(200, "text/plain", message );
   digitalWrite(led, 0);
     
 }
 
-void handleNotFound() {
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
-}
-
 void setup(void) {
+
   pinMode(led, OUTPUT);
   digitalWrite(led, 0);
   Serial.begin(115200);
+
+  // MAC address
+  uint8_t baseMac[6];
+  esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
+  char baseMacChr[18] = {0};
+  sprintf(baseMacChr, "%02X:%02X:%02X:%02X:%02X:%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
+  Serial.println("");
+  Serial.print("MAC Address => ");
+  Serial.println(baseMacChr);
+  
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  Serial.println("");
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
@@ -93,17 +79,12 @@ void setup(void) {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
+  // http://esp32.local/
   if (MDNS.begin("esp32")) {
     Serial.println("MDNS responder started");
   }
 
   server.on("/", handleRoot);
-
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-  });
-
-  server.onNotFound(handleNotFound);
 
   server.begin();
   Serial.println("HTTP server started");
