cf0c2278125c0ee59a8d86917cd94acd609b1986
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-20 08:24:50 +0900

    Create read_serial.php

---
+++ php/serial/read_serial.php
@@ -0,0 +1,36 @@
+<?php
+
+// https://github.com/lepiaf/serialport/blob/master/example/arduino.php
+//
+// php read_serial.php ;
+
+require_once './vendor/autoload.php';
+
+use lepiaf\SerialPort\SerialPort;
+use lepiaf\SerialPort\Parser\SeparatorParser;
+use lepiaf\SerialPort\Configure\TTYConfigure;
+
+//change baud rate
+$configure = new TTYConfigure();
+$configure->removeOption("9600");
+$configure->setOption("115200");
+
+$serialPort = new SerialPort(new SeparatorParser("\n"), $configure);
+$serialPort->open("/dev/ttyUSB0");
+
+$addMoreCountOnce = true;
+
+while ($data = $serialPort->read()) {
+  var_dump($data);
+
+  if ($data === "END COUNT") {
+    if ($addMoreCountOnce === false) {
+      break;
+    }
+
+    $serialPort->write("10\n");
+    $addMoreCountOnce = false;
+  }
+}
+
+$serialPort->close();
