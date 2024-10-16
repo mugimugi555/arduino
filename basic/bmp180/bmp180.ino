d601c59068c982600751797ab06cdbf798515366
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2024-10-16 19:46:48 +0900

    Update bmp180.ino

--- basic/bmp180/bmp180.ino
+++ basic/bmp180/bmp180.ino
@@ -1,54 +1,32 @@
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
 
-//
 #define seaLevelPressure_hPa 1013.25
 
 Adafruit_BMP085 bmp;
 
-//
 void setup() {
-
-  //
   Serial.begin(115200);
 
-  //
   if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   }
 
+  // CSVのヘッダーを出力
+  Serial.println("Temperature (C),Pressure (Pa),Altitude (m),Pressure at Sealevel (Pa),Real Altitude (m)");
 }
 
-//
 void loop() {
-
-  //
-  Serial.print("Temperature = ");
+  // センサーデータをCSV形式で出力
   Serial.print(bmp.readTemperature());
-  Serial.println(" *C");
-
-  //
-  Serial.print("Pressure = ");
+  Serial.print(",");
   Serial.print(bmp.readPressure());
-  Serial.println(" Pa");
-
-  //
-  Serial.print("Altitude = ");
+  Serial.print(",");
   Serial.print(bmp.readAltitude());
-  Serial.println(" meters");
-
-  //
-  Serial.print("Pressure at sealevel (calculated) = ");
+  Serial.print(",");
   Serial.print(bmp.readSealevelPressure());
-  Serial.println(" Pa");
+  Serial.print(",");
+  Serial.println(bmp.readAltitude(seaLevelPressure_hPa * 100));
 
-  //
-  Serial.print("Real altitude = ");
-  Serial.print(bmp.readAltitude(seaLevelPressure_hPa * 100));
-  Serial.println(" meters");
-
-  //
-  Serial.println();
   delay(500);
-
 }
