14a03baaf9df86b5befc9d900838a37f155e3afd
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2024-10-16 17:00:43 +0900

    Update install.sh

--- php/serial/install.sh
+++ php/serial/install.sh
@@ -11,7 +11,6 @@ sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer ;
 composer -v ;
 
 # install php serial
-cd ;
 composer require "lepiaf/serialport" ;
 
 # set tttyUSB0 permission
@@ -23,4 +22,4 @@ composer require "lepiaf/serialport" ;
 echo 'KERNEL=="tty[A-Z]*[0-9]|pppox[0-9]*|ircomm[0-9]*|noz[0-9]*|rfcomm[0-9]*", GROUP="dialout", MODE="0666"' | sudo tee /etc/udev/rules.d/50-udev-default.rules ;
 
 # do php serial
-wget https://raw.githubusercontent.com/mugimugi555/arduino/main/php/serial/read_serial.php && php read_serial.php ;
+php read_serial.php ;
