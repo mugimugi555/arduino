6e8c38dfb88e010473f2a62773fa48529e1e3ee2
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2024-10-16 22:25:42 +0900

    Create web_moisture.ino

---
+++ esp8266/web_moisture/web_moisture.ino
@@ -0,0 +1,96 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>  // mDNSライブラリのインクルード
+
+// 湿度センサーのピン設定
+const int moisturePin = A0;  // アナログピンに接続
+
+// WiFi SSIDとパスワードをホスト名を指定
+const char* ssid     = "WIFISSID"  ;
+const char* password = "WIFIPASSWD";
+const char* hostname = "HOSTNAME"  ;
+
+ESP8266WebServer server(80); // Webサーバーのポート設定
+
+void setup() {
+  Serial.begin(115200); // シリアル通信を開始
+  connectToWiFi(); // WiFiに接続
+  server.on("/", handleRoot); // ルートURLのハンドラ設定
+  server.begin(); // サーバーを開始
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient(); // クライアントからのリクエストを処理
+}
+
+//
+void connectToWiFi() {
+
+  // Connect to WiFi
+  WiFi.hostname(hostname);
+  WiFi.begin(ssid, password);
+
+  // Wait for WiFi connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+
+  // Display the ESP32's hostname
+  Serial.print("Hostname: http://");
+  Serial.print(WiFi.getHostname());
+  Serial.println(".local");
+
+  // Display the ESP32's IP address
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Display subnet mask
+  Serial.print("Subnet Mask: ");
+  Serial.println(WiFi.subnetMask());
+
+  // Display gateway IP
+  Serial.print("Gateway IP: ");
+  Serial.println(WiFi.gatewayIP());
+
+  // Display DNS server IP
+  Serial.print("DNS IP: ");
+  Serial.println(WiFi.dnsIP());
+
+  // Display the ESP32's MAC address
+  Serial.print("MAC address: ");
+  Serial.println(WiFi.macAddress());
+
+  // Start mDNS responder
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+
+}
+
+// ルートURLへのリクエストを処理する関数
+void handleRoot() {
+  // 湿度センサーの値を取得
+  int moistureValue = analogRead(moisturePin);
+  
+  // パーセントで計算
+  float moisturePercent = map(moistureValue, 0, 1024, 0, 100);
+  
+  // JSON形式でデータを作成
+  String json = "{\"moisture\":";
+  json += String(moisturePercent);
+  json += "}";
+
+  // クライアントにJSONデータを送信
+  server.send(200, "application/json", json);
+  
+  // シリアル出力
+  Serial.print("Soil Moisture: ");
+  Serial.print(moisturePercent);
+  Serial.println("%");
+}
