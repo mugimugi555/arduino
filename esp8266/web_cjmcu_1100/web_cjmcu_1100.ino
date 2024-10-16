08ff2a099571ef0f4f20c758c74e3fe00f6fb2a5
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2024-10-16 20:07:15 +0900

    Update web_cjmcu_1100.ino

--- esp8266/web_cjmcu_1100/web_cjmcu_1100.ino
+++ esp8266/web_cjmcu_1100/web_cjmcu_1100.ino
@@ -18,32 +18,14 @@ ESP8266WebServer server(80);
 void setup(void) {
   
   //
+  Serial.begin(115200);
+
+  //
   pinMode(led, OUTPUT);
   digitalWrite(led, 0);
 
   //
-  Serial.begin(115200);
-
-  // Wi-Fi接続
-  WiFi.hostname(hostname);
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-  Serial.println("\nWiFi connected");
-
-  // ESP8266のIPアドレスを表示
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
-
-  // mDNSサービスを開始
-  if (MDNS.begin(hostname)) {
-    Serial.println("MDNS responder started");
-  } else {
-    Serial.println("Error setting up MDNS responder!");
-  }
+  connectToWiFi();
 
   //
   server.on("/", handleRoot);
@@ -61,6 +43,55 @@ void setup(void) {
 void loop(void) {
   server.handleClient();
   MDNS.update();
+}
+
+//
+void connectToWiFi() {
+
+  // Connect to WiFi
+  WiFi.hostname(hostname);
+  WiFi.begin(ssid, password);
+
+  // Wait for WiFi connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+
+  // Display the ESP32's hostname
+  Serial.print("Hostname: http://");
+  Serial.print(WiFi.getHostname());
+  Serial.println(".local");
+
+  // Display the ESP32's IP address
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Display subnet mask
+  Serial.print("Subnet Mask: ");
+  Serial.println(WiFi.subnetMask());
+
+  // Display gateway IP
+  Serial.print("Gateway IP: ");
+  Serial.println(WiFi.gatewayIP());
+
+  // Display DNS server IP
+  Serial.print("DNS IP: ");
+  Serial.println(WiFi.dnsIP());
+
+  // Display the ESP32's MAC address
+  Serial.print("MAC address: ");
+  Serial.println(WiFi.macAddress());
+
+  // Start mDNS responder
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+
 }
 
 //
