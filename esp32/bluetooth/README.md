e164d5a4f45f17a97e790d767fb2724da699d1eb
mugimugi555 <harukomugi@gmail.com>
2024-10-18 14:36:05 +0900

    リファクタリング

--- esp32/bluetooth/README.md
+++ esp32/bluetooth/README.md
@@ -1,1 +1,34 @@
-test
+# Install Arduino CLI
+````bash
+cd
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+source ~/.bashrc
+````
+#
+````bash
+arduino-cli config init
+arduino-cli core update-index
+arduino-cli core list
+````
+# Install ESP32 board
+````bash
+arduino-cli config set board_manager.additional_urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+arduino-cli core update-index
+arduino-cli core install esp32:esp32
+arduino-cli core list
+````
+# Install Library
+````bash
+arduino-cli lib install "Adafruit BME280 Library"
+arduino-cli lib install "Adafruit BusIO"
+arduino-cli lib install "Adafruit Unified Sensor"
+arduino-cli lib install "DHT sensor library"
+arduino-cli lib install "ESP32 BLE Keyboard"
+arduino-cli lib install "ESP32 BLE Mouse"
+arduino-cli lib install "HX711 Arduino Library"
+````
+# Compile and Upload command
+````bash
+bash upload_esp32.sh web_dht11/web_dht11.ino wifissid wifipasswd hostname
+````
---
+++ esp32/bluetooth/upload_esp32.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# 引数の確認
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <sketch_path> <wifi_ssid> <wifi_password> <hostname>"
+    exit 1
+fi
+
+#
+SKETCH_PATH=$1      # Arduinoスケッチファイルのパス
+WIFI_SSID=$2        # WiFi SSID
+
+#
+BOARD_NAME=esp32:esp32:esp32
+SERIAL_BAND=115200
+
+# ランダムなフォルダを作成
+RANDOM_NAME=$(mktemp -u XXXXX)  # ランダムな名前を生成
+RANDOM_DIR="/tmp/$RANDOM_NAME"
+mkdir "$RANDOM_DIR"              # フォルダを作成
+TEMP_SKETCH="$RANDOM_DIR/$RANDOM_NAME.ino"  # 同じ名前のファイルを作成
+echo "Temporary sketch directory: $RANDOM_DIR"
+
+# WIFISSID、WIFIPASSWD、HOSTNAMEの置き換え
+sed -e "s/WIFISSID/$WIFI_SSID/" \
+    -e "s/WIFIPASSWD/$WIFI_PASSWORD/" \
+    -e "s/HOSTNAME/$HOSTNAME/" \
+    "$SKETCH_PATH" > "$TEMP_SKETCH"
+
+# 作成した一時スケッチファイルの内容を表示
+cat "$TEMP_SKETCH"
+echo "replace success => $TEMP_SKETCH"
+echo "now compiling..."
+
+# コンパイルとアップロード
+arduino-cli compile --fqbn "$BOARD_NAME" "$TEMP_SKETCH"
+if [ $? -eq 0 ]; then
+
+    echo "Compilation successful, uploading..."
+    arduino-cli upload -p /dev/ttyUSB0 --fqbn "$BOARD_NAME" "$TEMP_SKETCH"
+
+    if [ $? -eq 0 ]; then
+        # アップロード成功時に/dev/ttyUSB0の値を出力
+        echo "Upload successful!"
+        stty -F /dev/ttyUSB0 $SERIAL_BAND
+        cat /dev/ttyUSB0
+        #screen /dev/ttyUSB0 $SERIAL_BAND
+    else
+        echo "Upload failed"
+    fi
+
+else
+    echo "Compilation failed"
+fi
+
+# 一時ファイルを削除
+rm "$TEMP_SKETCH"
