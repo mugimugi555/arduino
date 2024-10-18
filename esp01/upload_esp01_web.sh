7484ae93370310e95689c4aa3c78ab2a7e1b451e
mugimugi555 <harukomugi@gmail.com>
2024-10-18 16:35:02 +0900

    リファクタリング

--- esp01/upload_esp01_web.sh
+++ esp01/upload_esp01_web.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# bash upload_esp01_web.sh web_bme280/web_bme280.ino wifissid wifipasswd hostname
+
 # 引数の確認
 if [ "$#" -ne 4 ]; then
     echo "Usage: $0 <sketch_path> <wifi_ssid> <wifi_password> <hostname>"
@@ -15,6 +17,7 @@ HOSTNAME=$4         # ESP01のホスト名
 # ESP01用のボード設定
 BOARD_NAME=esp8266:esp8266:generic
 SERIAL_BAND=115200
+SERIAL_PORT=/dev/ttyUSB0
 
 # ランダムなフォルダを作成
 RANDOM_NAME=$(mktemp -u XXXXX)  # ランダムな名前を生成
@@ -30,7 +33,7 @@ sed -e "s/WIFISSID/$WIFI_SSID/" \
     "$SKETCH_PATH" > "$TEMP_SKETCH"
 
 # 作成した一時スケッチファイルの内容を表示
-cat "$TEMP_SKETCH"
+#cat "$TEMP_SKETCH"
 echo "replace success => $TEMP_SKETCH"
 echo "now compiling..."
 
@@ -39,14 +42,13 @@ arduino-cli compile --fqbn "$BOARD_NAME" "$TEMP_SKETCH"
 if [ $? -eq 0 ]; then
 
     echo "Compilation successful, uploading..."
-    arduino-cli upload -p /dev/ttyUSB0 --fqbn "$BOARD_NAME" "$TEMP_SKETCH"
+    arduino-cli upload -p "$SERIAL_PORT" --fqbn "$BOARD_NAME" "$TEMP_SKETCH"
 
     if [ $? -eq 0 ]; then
         # アップロード成功時に/dev/ttyUSB0の値を出力
         echo "Upload successful!"
-        stty -F /dev/ttyUSB0 $SERIAL_BAND
-        cat /dev/ttyUSB0
-        #screen /dev/ttyUSB0 $SERIAL_BAND
+        #echo "start serial monitor [ exit : Ctrl + A X ]"
+        #picocom -b "$SERIAL_BAND" "$SERIAL_PORT"
     else
         echo "Upload failed"
     fi
