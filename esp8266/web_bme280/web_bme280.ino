6ad5cf607951499095200ce57234c55d8abe38b2
mugimugi555 <harukomugi@gmail.com>
2024-11-03 00:45:57 +0900

    リファクタリング

--- esp8266/web_bme280/web_bme280.ino
+++ esp8266/web_bme280/web_bme280.ino
@@ -7,22 +7,23 @@ arduino-cli core install esp8266:esp8266
 # このプログラムで必要なライブラリのインストール
 arduino-cli lib install "ESP8266WiFi"             # ESP8266ボード用のWiFi機能を提供するライブラリ。WiFi接続やアクセスポイントの作成に使用します。
 arduino-cli lib install "ESP8266mDNS"             # mDNS（マルチキャストDNS）を使用して、ESP8266デバイスをネットワークで簡単に見つけられるようにするライブラリ。
-arduino-cli lib install "ESP8266WebServer"        # ESP8266上でWebサーバー機能を実装するためのライブラリ。HTTPリクエストの処理やWebページの提供が可能です。
-arduino-cli lib install "ArduinoJson"             # JSON形式のデータを簡単に作成、解析するためのライブラリ。API通信やデータの保存に役立ちます。
-arduino-cli lib install "Adafruit BME280 Library" # Adafruit社のBME280センサー用ライブラリ。温度、湿度、気圧の取得をサポートします。
+arduino-cli lib install "ESPAsyncTCP"             # ESP8266用の非同期TCP通信を提供するライブラリ。非同期的に複数のクライアントと接続するために使用します。
+arduino-cli lib install "ESPAsyncWebServer"       # ESP8266用の非同期Webサーバーライブラリ。HTTPリクエストの処理やレスポンスを非同期に行うことができ、複数のクライアントからのリクエストに同時に対応できます。
+arduino-cli lib install "ArduinoJson"             # JSON形式のデータを簡単に作成、解析するためのライブラリ。API通信やデータの保存に役立ち、シリアライズやデシリアライズが簡単に行えます。
+arduino-cli lib install "Adafruit BME280 Library" # Adafruit社のBME280センサー用ライブラリ。温度、湿度、気圧のデータ取得をサポートし、I2C通信を介してセンサーから情報を取得できます。
 
 # コンパイルとアップロード例
 bash upload_esp8266_web.sh web_bme280/web_bme280.ino wifissid wifipasswd hostname
 
 */
 
-#include <ESP8266WiFi.h>          // ESP8266用のWiFi機能を提供するライブラリ。WiFi接続やアクセスポイントの作成に使用します。
-#include <ESP8266mDNS.h>          // mDNS（マルチキャストDNS）を使用するためのライブラリ。デバイスをネットワークで簡単に発見できるようにします。
-#include <ESP8266WebServer.h>     // ESP8266デバイスでWebサーバーを構築するためのライブラリ。HTTPリクエストの処理やWebページの提供が可能です。
-#include <ArduinoJson.h>          // JSON形式のデータを作成・解析するためのライブラリ。API通信やデータの保存に役立ちます。
-#include <Wire.h>                 // I2C通信を使用するための標準ライブラリ。BME280などのI2Cデバイスと通信するために必要です。
-#include <Adafruit_Sensor.h>      // Adafruit社製センサー用の統一インターフェースライブラリ。各種センサーからのデータ取得に役立ちます。
-#include <Adafruit_BME280.h>      // BME280センサーのためのライブラリ。温度、湿度、気圧のデータ取得が簡単に行えます。
+#include <ESP8266WiFi.h>       // ESP8266用のWiFi機能を提供するライブラリ。WiFi接続やアクセスポイントの作成に使用します。
+#include <ESP8266mDNS.h>       // mDNS（マルチキャストDNS）を使用するためのライブラリ。デバイスをネットワークで簡単に発見できるようにします。
+#include <ESPAsyncWebServer.h> // ESP8266用の非同期Webサーバーライブラリ。HTTPリクエストの処理を非同期で行い、複数のクライアントからのリクエストに同時に対応できるようにします。
+#include <ArduinoJson.h>       // JSON形式のデータを作成・解析するためのライブラリ。API通信やデータの保存に役立ち、データのシリアライズとデシリアライズが容易に行えます。
+#include <Wire.h>              // I2C通信を使用するための標準ライブラリ。BME280などのI2Cデバイスと通信するために必要で、マスターとスレーブ間のデータ送受信を管理します。
+#include <Adafruit_Sensor.h>   // Adafruit社製センサー用の統一インターフェースライブラリ。各種センサーからのデータ取得に役立ち、異なるセンサー間での互換性を提供します。
+#include <Adafruit_BME280.h>   // BME280センサーのためのライブラリ。温度、湿度、気圧のデータ取得が簡単に行え、センサーの初期化やデータ読み取りを簡素化します。
 
 // WiFi SSIDとパスワードをホスト名を指定
 const char* ssid     = "WIFISSID"  ; // 自分のWi-Fi SSIDに置き換える
@@ -30,17 +31,14 @@ const char* password = "WIFIPASSWD"; // 自分のWi-Fiパスワードに置き�
 const char* hostname = "HOSTNAME"  ; // ESP8266のホスト名 http://HOSTNAME.local/ でアクセスできるようになります。
 
 // センサーとの接続方法
-// BME280センサー       ESP8266
+// BME280センサー      ESP8266
 // VCC  <---------->  3.3V
 // GND  <---------->  GND
 // SDA  <---------->  D2 (GPIO 4)
 // SCL  <---------->  D1 (GPIO 5)
 
-// BME280オブジェクトの作成
-Adafruit_BME280 bme;
-
-// Webサーバー設定
-ESP8266WebServer server(80);
+Adafruit_BME280 bme;       // BME280オブジェクトの作成
+AsyncWebServer server(80); // 非同期Webサーバーの初期化
 
 //----------------------------------------------------------------------------
 // 初期実行
@@ -62,7 +60,10 @@ void setup() {
   connectToWiFi();
 
   // ルートURLへのハンドラを設定
-  server.on("/", handleRoot);
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    String jsonResponse = createJson();
+    request->send(200, "application/json", jsonResponse);
+  });
 
   // Webサーバーの開始
   server.begin();
@@ -82,9 +83,6 @@ void loop() {
     lastMillis = currentMillis;
     Serial.println(createJson());
   }
-
-  // クライアントリクエストを処理
-  server.handleClient();
 
   // ホスト名の更新
   MDNS.update();
@@ -162,25 +160,20 @@ void connectToWiFi() {
 // Webサーバー系
 //----------------------------------------------------------------------------
 
-// ルートURLにアクセスした際の処理
-void handleRoot() {
-
-  // HTTPレスポンスを送信
-  String jsonResponse = createJson();
-  server.send(200, "application/json", jsonResponse);
-
-}
-
 // 取得されるデータをJSON形式で生成
 String createJson() {
 
+  // 不快指数の計算
+  float discomfortIndex = temperature + 0.36 * humidity + 41.2;
+
   // JSONオブジェクトを作成
   StaticJsonDocument<200> doc;
-  doc["temperature"] = bme.readTemperature();       // 温度
-  doc["humidity"]    = bme.readHumidity();          // 湿度
-  doc["pressure"]    = bme.readPressure() / 100.0F; // PaをhPaに変換
-  doc["hostname"]    = hostname;                    // ホスト名
-  doc["ipaddress"]   = WiFi.localIP().toString();   // IPアドレス
+  doc["temperature"]     = bme.readTemperature();       // 温度
+  doc["humidity"]        = bme.readHumidity();          // 湿度
+  doc["discomfortIndex"] = discomfortIndex;             // 不快指数
+  doc["pressure"]        = bme.readPressure() / 100.0F; // PaをhPaに変換
+  doc["hostname"]        = hostname;                    // ホスト名
+  doc["ipaddress"]       = WiFi.localIP().toString();   // IPアドレス
 
   // JSONデータを文字列にシリアライズ
   String json;
